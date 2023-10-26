88eff3bafc9a8490403c6f0fec046cb0a488ecd6
phonglv <lovanphong2299@gmail.com>
2023-10-26 21:06:36 +0700

    Add project files.

---
+++ ToolFormatExcel/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace ToolFormatExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnFormat = new Button();
+            openFileDialog1 = new OpenFileDialog();
+            openFileDialog2 = new OpenFileDialog();
+            textBox1 = new TextBox();
+            richTextBox1 = new RichTextBox();
+            label1 = new Label();
+            label2 = new Label();
+            btnFileTemplate = new Button();
+            button3 = new Button();
+            SuspendLayout();
+            // 
+            // btnFormat
+            // 
+            btnFormat.Location = new Point(618, 87);
+            btnFormat.Name = "btnFormat";
+            btnFormat.Size = new Size(128, 39);
+            btnFormat.TabIndex = 0;
+            btnFormat.Text = "Format";
+            btnFormat.UseVisualStyleBackColor = true;
+            btnFormat.Click += btnFormat_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // openFileDialog2
+            // 
+            openFileDialog2.FileName = "openFileDialog2";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(173, 49);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(189, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(27, 173);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(453, 265);
+            richTextBox1.TabIndex = 2;
+            richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(27, 52);
+            label1.Name = "label1";
+            label1.Size = new Size(75, 15);
+            label1.TabIndex = 3;
+            label1.Text = "File template";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(27, 155);
+            label2.Name = "label2";
+            label2.Size = new Size(51, 15);
+            label2.TabIndex = 3;
+            label2.Text = "File xuất";
+            // 
+            // btnFileTemplate
+            // 
+            btnFileTemplate.Location = new Point(384, 52);
+            btnFileTemplate.Name = "btnFileTemplate";
+            btnFileTemplate.Size = new Size(75, 23);
+            btnFileTemplate.TabIndex = 4;
+            btnFileTemplate.Text = "Mở";
+            btnFileTemplate.UseVisualStyleBackColor = true;
+            btnFileTemplate.Click += btnFileTemplate_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(84, 151);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 4;
+            button3.Text = "button2";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(btnFileTemplate);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(richTextBox1);
+            Controls.Add(textBox1);
+            Controls.Add(btnFormat);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnFormat;
+        private OpenFileDialog openFileDialog1;
+        private OpenFileDialog openFileDialog2;
+        private TextBox textBox1;
+        private RichTextBox richTextBox1;
+        private Label label1;
+        private Label label2;
+        private Button btnFileTemplate;
+        private Button button3;
+    }
+}
---
+++ ToolFormatExcel/Form1.cs
@@ -0,0 +1,126 @@
+﻿using OfficeOpenXml;
+
+namespace ToolFormatExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFormat_Click(object sender, EventArgs e)
+        {
+            // Đường dẫn đến tệp Excel mẫu của bạn
+            string templatePath = @"C:\Users\lovan\Downloads\2022-2023 CN\1A.xlsx";
+
+            // Thư mục chứa các tệp dữ liệu của bạn
+            string dataDirectory = @"C:\Users\lovan\Downloads\2022-2023 CN"; // Cập nhật với đường dẫn thư mục dữ liệu của bạn
+
+            // Thư mục đầu ra mà bạn muốn lưu trữ các tệp Excel đã được định dạng
+            string outputDirectory = @"C:\Users\lovan\Downloads\ThuMucXuat"; // Cập nhật với đường dẫn thư mục đầu ra của bạn
+
+            // Đảm bảo thư mục đầu ra tồn tại
+            Directory.CreateDirectory(outputDirectory);
+
+            // Danh sách các tệp dữ liệu cần xử lý
+            List<string> dataFiles = Directory.GetFiles(dataDirectory, "*.xlsx").ToList();
+
+
+            // Load định dạng từ tệp Excel mẫu
+            FileInfo templateFile = new FileInfo(templatePath);
+            using (var templatePackage = new ExcelPackage(templateFile))
+            {
+                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                ExcelWorksheet templateWorksheet = templatePackage.Workbook.Worksheets[0]; // Chỉ mục của bảng tính trong tệp mẫu
+
+                // Lặp qua mỗi tệp dữ liệu và áp dụng định dạng từ tệp mẫu
+                //foreach (string dataFile in dataFiles)
+                //{
+                //    FileInfo dataFileInfo = new FileInfo(dataFile);
+
+                //    using (var dataPackage = new ExcelPackage(dataFileInfo))
+                //    {
+                //        ExcelWorksheet dataWorksheet = dataPackage.Workbook.Worksheets[0]; // Chỉ mục của bảng tính trong tệp dữ liệu
+
+                //        // Sao chép định dạng từ tệp mẫu và áp dụng cho tệp dữ liệu
+                //        for (int row = 1; row <= templateWorksheet.Dimension.End.Row; row++)
+                //        {
+                //            for (int col = 1; col <= templateWorksheet.Dimension.End.Column; col++)
+                //            {
+                //                ExcelRangeBase templateCell = templateWorksheet.Cells[row, col];
+                //                ExcelRangeBase dataCell = dataWorksheet.Cells[row, col];
+
+                //                // Sao chép định dạng (kiểu dáng) từ tệp mẫu và áp dụng cho tệp dữ liệu
+                //                dataCell.Style.Font.Bold = templateCell.Style.Font.Bold;
+                //                dataCell.Style.Numberformat.Format = templateCell.Style.Numberformat.Format;
+                //                dataCell.Style.Fill.PatternType = templateCell.Style.Fill.PatternType;
+                //                // Sao chép định dạng cột từ tệp mẫu và áp dụng cho tệp dữ liệu
+                //                dataWorksheet.Column(col).Width = templateWorksheet.Column(col).Width;
+                //                dataWorksheet.Column(col).Style.Font.Size = templateWorksheet.Column(col).Style.Font.Size;
+                //                dataWorksheet.Column(col).Style.Font.Name = templateWorksheet.Column(col).Style.Font.Name;
+                //                //// Chuyển đổi màu nền từ ExcelColor sang Color
+                //                //dataCell.Style.Fill.BackgroundColor.SetColor(System.Drawing.Color.FromArgb(templateCell.Style.Fill.BackgroundColor.Rgb));
+                //            }
+                //        }
+
+                //        // Lưu tệp dữ liệu đã được định dạng vào thư mục đầu ra
+                //        string outputFileName = Path.Combine(outputDirectory, dataFileInfo.Name);
+                //        dataPackage.SaveAs(new FileInfo(outputFileName));
+                //    }
+                //}\
+
+                // Lặp qua mỗi tệp dữ liệu và áp dụng định dạng từ tệp mẫu
+                foreach (string dataFile in dataFiles)
+                {
+                    FileInfo dataFileInfo = new FileInfo(dataFile);
+
+                    using (var dataPackage = new ExcelPackage(dataFileInfo))
+                    {
+                        ExcelWorksheet dataWorksheet = dataPackage.Workbook.Worksheets[0]; // Chỉ mục của bảng tính trong tệp dữ liệu
+
+                        // Sao chép định dạng cột (bao gồm cả kiểu chữ) từ tệp mẫu và áp dụng cho tệp dữ liệu
+                        for (int col = 1; col <= templateWorksheet.Dimension.End.Column; col++)
+                        {
+                            var templateColumnStyle = templateWorksheet.Column(col).Style;
+                            dataWorksheet.Column(col).Width = templateWorksheet.Column(col).Width;
+
+                            for (int row = 1; row <= dataWorksheet.Dimension.End.Row; row++)
+                            {
+                                var templateCell = templateWorksheet.Cells[row, col];
+                                var dataCell = dataWorksheet.Cells[row, col];
+
+                                // Sao chép tất cả thuộc tính định dạng từ tệp mẫu sang tệp dữ liệu
+                                dataCell.Style.Font.Name = templateCell.Style.Font.Name;
+                                dataCell.Style.Font.Size = templateCell.Style.Font.Size;
+                                dataCell.Style.Font.Bold = templateCell.Style.Font.Bold;
+                                dataCell.Style.Font.Italic = templateCell.Style.Font.Italic;
+                                dataCell.Style.Font.UnderLine = templateCell.Style.Font.UnderLine;
+                                dataCell.Style.Font.Strike = templateCell.Style.Font.Strike;                              
+                                dataCell.Style.Fill.PatternType = templateCell.Style.Fill.PatternType;
+                                dataCell.Style.Fill.PatternType = templateCell.Style.Fill.PatternType;
+                                //dataCell.Style.Border.Top.Style = templateCell.Style.Border.Top.Style;                         
+                                //dataCell.Style.Border.Left.Style = templateCell.Style.Border.Left.Style;                            
+                                //dataCell.Style.Border.Right.Style = templateCell.Style.Border.Right.Style;                              
+                                //dataCell.Style.Border.Bottom.Style = templateCell.Style.Border.Bottom.Style;
+                                dataCell.Style.TextRotation = templateCell.Style.TextRotation;
+
+                            }
+                        }
+
+                        // Lưu tệp dữ liệu đã được định dạng vào thư mục đầu ra
+                        string outputFileName = Path.Combine(outputDirectory, dataFileInfo.Name);
+                        dataPackage.SaveAs(new FileInfo(outputFileName));
+                    }
+                }
+            }
+
+            Console.WriteLine("Tất cả các tệp Excel đã được định dạng và đã được lưu.");
+        }
+
+        private void btnFileTemplate_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
